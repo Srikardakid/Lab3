6b3de120852e27903ff8e5bbf0e10c89e59cdbf8
Srikar <saisrikar.24@ichat.sp.edu.sg>
2025-11-09 14:16:25 +0800

    bmi testing

---
+++ Test_bmi.py
@@ -0,0 +1,9 @@
+import Lab2.bmi as bmi
+print("Test lab2 bmi")
+
+def test_bmi_normal_weight():
+    assert calculate_bmi(1.77,59) == 0
+def test_bmi_over_weight():
+    assert calculate_bmi(1.77,90) == 1
+def test_bmi_under_weight():
+    assert calculate_bmi(1.77,50) == -1

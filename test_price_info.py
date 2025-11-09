d4863d82c0b5d4878007fda72034d881203c273b
Srikar <saisrikar.24@ichat.sp.edu.sg>
2025-11-09 20:50:00 +0800

    test price units

---
+++ test_price_info.py
@@ -0,0 +1,8 @@
+import price_info
+def test_total_cost_shopping():
+    
+   result = price_info.total_cost_shopping()
+   assert result == 46.75
+def test_total_cost_fruits():
+   arr = price_info.cost_of_fruits('apple',10)
+   assert arr == 12

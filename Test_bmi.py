975be076ea461fd0a2fe7214a35e76ad03c60f85
Srikar <saisrikar.24@ichat.sp.edu.sg>
2025-11-09 17:09:45 +0800

    Did the tests for everything

--- Test_bmi.py
+++ Test_bmi.py
@@ -2,8 +2,8 @@ import Lab2.bmi as bmi
 print("Test lab2 bmi")
 
 def test_bmi_normal_weight():
-    assert calculate_bmi(1.77,59) == 0
+    assert bmi.calculate_bmi(1.77,59) == 0
 def test_bmi_over_weight():
-    assert calculate_bmi(1.77,90) == 1
+    assert bmi.calculate_bmi(1.77,90) == 1
 def test_bmi_under_weight():
-    assert calculate_bmi(1.77,50) == -1
+    assert bmi.calculate_bmi(1.77,50) == -1
--- test_Lab3.py
+++ test_Lab3.py
@@ -28,3 +28,19 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+def test_too_many_numbers():
+    result = []
+    input_arr = [64,34,25,12,22,11,90,67,69,53]
+
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_ASCENDING)
+    assert (result == 1)
+def test_bubblesort_no_numbers():
+    result = []
+    input_arr = []
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_ASCENDING)
+    assert (result == 0)
+def test_if_integer():
+    result = []
+    input_arr = ["hi",69.7]
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_ASCENDING)
+    assert (result == 2)
